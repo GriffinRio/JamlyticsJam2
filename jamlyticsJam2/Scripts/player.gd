1b018c57fb13ecf3b78f3e3aad7a99a9c73cc991
GriffinRio <griffinator03@gmail.com>
2026-05-21 18:02:35 -0700

    Dinky transformations animations setup

--- jamlyticsJam2/Scripts/player.gd
+++ jamlyticsJam2/Scripts/player.gd
@@ -2,17 +2,23 @@ extends CharacterBody2D
 
 const SPEED = 70.0
 const JUMP_VELOCITY = -400.0
-
-var type = ""
-var eating = false
-var stomach = {
-	"Fire": 0,
-	"Water": 0,
-	"Earth": 0,
+const element_types = {
+	[1,0,0]: "Fire",
+	[0,1,0]: "Water",
+	[0,0,1]: "Earth",
+	[1,1,0]: "Steam",
+	[1,0,1]: "Magma",
+	[0,1,1]: "Mud",
+	[1,1,1]: "Full",
 }
 
+var type = "Neutral"
+var eating = false
+var stomach = [0, 0, 0] #[Fire, Water, Earth]
+
 func _ready() -> void:
-	$Pivot/AnimatedSprite2D.animation = "Idle"
+	#Starts animator on Nuetral_Idle everytime
+	$Pivot/AnimatedSprite2D.animation = type +  "_Idle"
 	$Pivot/AnimatedSprite2D.play()
 
 func _physics_process(delta: float) -> void:
@@ -23,10 +29,10 @@ func _physics_process(delta: float) -> void:
 	if(!eating):
 		var direction = Input.get_axis("move_left", "move_right")
 		if direction:
-			$Pivot/AnimatedSprite2D.animation = type + "Walk"
+			$Pivot/AnimatedSprite2D.animation = type + "_Walk"
 			velocity.x = direction * SPEED
 		else:
-			$Pivot/AnimatedSprite2D.animation = type + "Idle"
+			$Pivot/AnimatedSprite2D.animation = type + "_Idle"
 			velocity.x = move_toward(velocity.x, 0, SPEED)
 		if(direction < 0):
 			$Pivot.scale.x = -1 
@@ -34,18 +40,34 @@ func _physics_process(delta: float) -> void:
 			$Pivot.scale.x = 1
 	
 		move_and_slide()
-	
+
+func update_type(element):
+	match element:
+		"Fire":
+			stomach[0] = 1
+		"Water":
+			stomach[1] = 1
+		"Earth":
+			stomach[2] = 1
+		_:
+			print("Error: Unkown element")
+	type = element_types[stomach]
+	print(type)
+		
+
 func _on_mouth_body_entered(body: Node2D) -> void:
 	if(body.is_in_group("Elementals")):
 		eating = true
 		if(body.element == "Fire"):
-			$Pivot/AnimatedSprite2D.animation = type + "Chomp"
+			$Pivot/AnimatedSprite2D.animation = type + "_Chomp"
 		else:
-			$Pivot/AnimatedSprite2D.animation = type + "Big_Chomp"
+			$Pivot/AnimatedSprite2D.animation = type + "_Big_Chomp"
 		await $Pivot/AnimatedSprite2D.animation_finished
-		$Pivot/AnimatedSprite2D.animation = type + "Idle"
+		update_type(body.element)
+		$Pivot/AnimatedSprite2D.animation = type + "_Idle"
 		$Pivot/AnimatedSprite2D.play()
 		eating = false
-		stomach[body.element] += 1
+		
 		body.queue_free()
+		
 		
