6d71798a80a0a0f753df81f7f236cafc0fafc995
GriffinRio <griffinator03@gmail.com>
2026-05-21 17:20:06 -0700

    Setup 3 types of elementals

--- jamlyticsJam2/Scripts/elemental.gd
+++ jamlyticsJam2/Scripts/elemental.gd
@@ -2,20 +2,11 @@ extends RigidBody2D
 
 @export_enum("Fire", "Water", "Earth") var element: String
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	match element:
-		"Fire":
-			$MeshInstance2D.modulate = Color("red")
-		"Water":
-			$MeshInstance2D.modulate = Color("blue")
-		"Earth":
-			$MeshInstance2D.modulate = Color("green")
-		_:
-			$MeshInstance2D.modulate = Color("pink")
+	if (element == ""):
+			print("ERROR: No type chosen")
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
